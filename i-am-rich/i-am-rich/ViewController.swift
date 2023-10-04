95c7ae97973cc5e2b54f9e5e808de7861082f71d
Jomar Leaño <69794504+therealmai@users.noreply.github.com>
2023-10-04 21:34:07 +0800

    first commit

---
+++ i-am-rich/i-am-rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i-am-rich
+//
+//  Created by Jomar Leano on 10/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
